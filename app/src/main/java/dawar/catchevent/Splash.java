89c12a74b90ce80f98ee641d05a05b8cb3d864a1
shivamdawar97 <shivam97.dawar@gmail.com>
2018-03-12 22:15:36 +0530

    Modified 13

--- app/src/main/java/dawar/catchevent/Splash.java
+++ app/src/main/java/dawar/catchevent/Splash.java
@@ -25,6 +25,7 @@ public class Splash extends AppCompatActivity {
             new Handler().postDelayed(new Runnable(){
                 @Override
                 public void run() {
+
                 /* Create an Intent that will start the Menu-Activity. */
                     Intent mainIntent = new Intent(Splash.this,MainActivity.class);
                     Splash.this.startActivity(mainIntent);
---
+++ app/src/main/java/dawar/catchevent/UdbhavMainActivity.java
@@ -0,0 +1,13 @@
+package dawar.catchevent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UdbhavMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_udbhav_main);
+    }
+}
